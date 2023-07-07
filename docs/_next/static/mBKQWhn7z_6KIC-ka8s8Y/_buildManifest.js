29dbc7b8908ccc61422b1f3b77091f8c2ed6b721
Carlos Fonseca <carlos@tookstock.com>
2023-07-07 00:38:20 -0600

    chore: first load

---
+++ docs/_next/static/mBKQWhn7z_6KIC-ka8s8Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/243-2bf98c6ae2234275.js","static/chunks/pages/index-129993b1633338b0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
